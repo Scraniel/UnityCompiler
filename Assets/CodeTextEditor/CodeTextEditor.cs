e8faa4db45d026f213d711e7e67a6534b28f8c59
Danny Lewis <nitomatik@gmail.com>
2017-06-11 18:07:03 -0700

    Refactored, added output window

---
+++ Assets/CodeTextEditor/CodeTextEditor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeTextEditor : MonoBehaviour
+{
+
+    [SerializeField]
+    private string DefaultText;
+
+    [SerializeField]
+    private InputField OutputField;
+
+    private InputField _inputField;
+    private Color _defaultTextColour;
+
+    public void Start()
+    {
+        _inputField = gameObject.GetComponent<InputField>();
+        _inputField.text = DefaultText.Replace("\\n", "\n").Replace("\\t", "\t");
+        _defaultTextColour = OutputField.textComponent.color;
+    }
+
+    public void Compile()
+    {
+        OutputField.textComponent.color = _defaultTextColour;
+        CompilerResults compile = CompileHelper.CompileCodeFromString(_inputField.text);
+
+        if (compile.Errors.HasErrors)
+        {
+            string text = "Compile error: ";
+            foreach (CompilerError error in compile.Errors)
+            {
+                text += '\n' + error.ToString();
+            }
+
+            var defaultColour = OutputField.textComponent.color;
+            OutputField.textComponent.color = Color.red;
+            OutputField.text = text;
+            return;
+        }
+
+        Module module = compile.CompiledAssembly.GetModules()[0];
+        Type typeInfo = null;
+        MethodInfo methodInfo = null;
+
+        if (module != null)
+        {
+            typeInfo = module.GetType("TestClass");
+        }
+
+        if (typeInfo != null)
+        {
+            methodInfo = typeInfo.GetMethod("EchoStuff");
+        }
+
+        if (methodInfo != null)
+        {
+            OutputField.text = (string)methodInfo.Invoke(null, new object[] { "Here's some stuff!" });
+        }
+
+    }
+}
---
+++ Assets/CodeTextEditor/CompileHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+public class CompileHelper
+{
+    public static CompilerResults CompileCodeFromString(string code, string[] references = null, CompilerParameters CompilerParams = null)
+    {
+
+        if (CompilerParams == null)
+        {
+            CompilerParams = new CompilerParameters();
+            CompilerParams.GenerateInMemory = true;
+            CompilerParams.TreatWarningsAsErrors = false;
+            CompilerParams.GenerateExecutable = false;
+            CompilerParams.CompilerOptions = "/optimize";
+        }
+
+        if (references != null)
+        {
+            CompilerParams.ReferencedAssemblies.AddRange(references);
+        }
+
+        CSharpCodeProvider provider = new CSharpCodeProvider();
+        return  provider.CompileAssemblyFromSource(CompilerParams, code);
+    }
+	
+}
